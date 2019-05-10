e0ec100079a78c7abc79925c59c7405ea3b50a82
凌辰 <807868486@qq.com>
2019-05-11 01:19:53 +0800

    项目第一次提交

---
+++ src/main/java/com/linn/springboot/SpringBootWebRestfulcrudApplication.java
@@ -0,0 +1,16 @@
+package com.linn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 这是一个SpringBoot启动类
+ */
+@SpringBootApplication
+public class SpringBootWebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebRestfulcrudApplication.class, args);
+    }
+
+}
